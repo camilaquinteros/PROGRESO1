faaed1eeb31f4c89a3f395d69786fc018fb098fa
camilaquinteros <130692640+camilaquinteros@users.noreply.github.com>
2023-05-02 12:53:06 -0500

    Update examenprogreso1.c

--- examenprogreso1.c
+++ examenprogreso1.c
@@ -9,30 +9,6 @@ int encontrarMayor(int numeros[], int n) {
         }
     }
     return mayor;
-}
 
-// Creo la función principal
-int main() {
-    int n;
-    printf("Ingrese el número de datos de entrada: ");
-    scanf("%d", &n);
-
-    // Creo una matriz para almacenar a todos los números enteros
-    int numeros[n];
-
-    // Aquí se pide al usuario que ingrese los números enteros
-    printf("Ingrese los números enteros: ");
-    for (int i = 0; i < n; i++) {
-        scanf("%d", &numeros[i]);
-    }
-
-    // Se llama a la función para encontrar el número mayor
-    int mayor = encontrarMayor(numeros, n);
-
-    // Procede a imprimir el resultado
-    printf("El número mayor es: %d\n", mayor);
-
-    return 0;
-}
 
 
